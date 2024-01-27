a28852d5f13e465d283cfdfc62d3400158a68995
natanbc <natanbc@users.noreply.github.com>
2024-01-27 03:28:23 +0000

    live-iso: don't update microcode, breaks ventoy boot

--- hosts/live-iso/hardware-configuration.nix
+++ hosts/live-iso/hardware-configuration.nix
@@ -1,6 +1,7 @@
 {
   nixpkgs.hostPlatform = "x86_64-linux";
-  hardware.cpu.amd.updateMicrocode = true;
-  hardware.cpu.intel.updateMicrocode = true;
+  # Breaks boot at least on ventoy
+  #hardware.cpu.amd.updateMicrocode = true;
+  #hardware.cpu.intel.updateMicrocode = true;
 }
 
