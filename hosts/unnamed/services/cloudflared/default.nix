fb24da3235371f8f25577b0bf9c4f175c3b24414
natanbc <natanbc@users.noreply.github.com>
2024-01-29 01:05:23 +0000

    unnamed: cloudflared tunnel uuid

--- hosts/unnamed/services/cloudflared/default.nix
+++ hosts/unnamed/services/cloudflared/default.nix
@@ -9,7 +9,7 @@
   services.cloudflared = {
     enable = true;
     tunnels = {
-      "00000000-0000-0000-0000-000000000000" = {
+      "2a26f50f-d803-41e9-b3a4-a34c8b5104a2" = {
         credentialsFile = config.age.secrets.cloudflared-credentials.path;
         ingress = {
           "mastodon.natanbc.net" = {
