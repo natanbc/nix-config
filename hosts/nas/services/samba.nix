9519940961454670120534a3bcb83eb29a4f1b3d
natanbc <natanbc@users.noreply.github.com>
2024-08-17 04:10:06 +0000

    nas: disable test share

--- hosts/nas/services/samba.nix
+++ hosts/nas/services/samba.nix
@@ -11,24 +11,10 @@
     '';
 
     shares = {
-      # Test share, just to measure performance
-      test = {
-        path = "/tmp/samba-test";
-        browseable = "yes";
-        "force user" = config.users.users.samba-test.name;
-        "guest ok" = "yes";
-        writeable = "yes";
-      };
     };
   };
 
   systemd.services.samba-smbd.after = [
     config.systemd.services.tailscaled.name
   ];
-
-  users.users.samba-test = {
-    isSystemUser = true;
-    group = config.users.groups.samba-test.name;
-  };
-  users.groups.samba-test = {};
 }
