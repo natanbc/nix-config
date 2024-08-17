a4ee524e7298a061d8ba7b758371ff5d84fcbe54
natanbc <natanbc@users.noreply.github.com>
2024-08-17 02:12:11 +0000

    nas: use unstable tailscale

--- hosts/nas/services/tailscale.nix
+++ hosts/nas/services/tailscale.nix
@@ -1,8 +1,13 @@
+{ inputs, pkgs, ... }:
 {
   services.tailscale = {
     enable = true;
     extraSetFlags = ["--advertise-exit-node"];
     openFirewall = true;
     useRoutingFeatures = "both";
+
+    package = (import inputs.nixpkgs-unstable {
+      system = pkgs.system;
+    }).tailscale;
   };
 }
