402ba5de21b04c5f4d96f41c3c1d975ccc6643ce
natanbc <natanbc@users.noreply.github.com>
2024-09-18 21:29:13 +0000

    all: nginx: fix low address bits warning

--- hosts/common/optional/nginx.nix
+++ hosts/common/optional/nginx.nix
@@ -15,7 +15,7 @@
     recommendedTlsSettings = true;
 
     commonHttpConfig = ''
-      set_real_ip_from 127.0.0.1/8;
+      set_real_ip_from 127.0.0.0/8;
       set_real_ip_from ::1/128;
       real_ip_header CF-Connecting-IP;
 
