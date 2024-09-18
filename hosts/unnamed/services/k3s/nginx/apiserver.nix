c6058272ab5d958cc0243938f3ce9d705ca99715
natanbc <natanbc@users.noreply.github.com>
2024-09-18 21:28:43 +0000

    unnamed: fix kubevirt console

--- hosts/unnamed/services/k3s/nginx/apiserver.nix
+++ hosts/unnamed/services/k3s/nginx/apiserver.nix
@@ -7,6 +7,10 @@
         proxyPass = "https://127.0.0.1:6443";
         extraConfig = ''
           proxy_ssl_verify off;
+
+          proxy_set_header Upgrade $http_upgrade;
+          # Defined in hosts/common/optional/nginx.nix
+          proxy_set_header Connection $connection_upgrade;
         '';
       };
     };
