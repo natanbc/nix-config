c093ff4086d861ab7ac350567da854018fde219d
natanbc <natanbc@users.noreply.github.com>
2024-09-19 21:45:28 +0000

    unnamed: docker-registry: disable request buffering, move request body size limit to location scope

--- hosts/unnamed/services/docker-registry/nginx.nix
+++ hosts/unnamed/services/docker-registry/nginx.nix
@@ -1,15 +1,20 @@
 { config, ... }:
 {
-  services.nginx.clientMaxBodySize = "100M";
   services.nginx.virtualHosts = {
     "docker-registry.natanbc.net" = {
       addSSL = true;
       enableACME = true;
-      locations."/".proxyPass =
-        let
-          ip = config.services.dockerRegistry.listenAddress;
-          port = toString config.services.dockerRegistry.port;
-        in "http://${ip}:${port}";
+      locations."/" = {
+        proxyPass =
+          let
+            ip = config.services.dockerRegistry.listenAddress;
+            port = toString config.services.dockerRegistry.port;
+          in "http://${ip}:${port}";
+        extraConfig = ''
+          proxy_request_buffering off;
+          client_max_body_size 5G;
+        '';
+      };
     };
   };
 }
