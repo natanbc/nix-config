a072a6e8346680b1d8bbd0a8b4308d7b4ebc2688
natanbc <natanbc@users.noreply.github.com>
2024-09-19 20:57:01 +0000

    all: tailscale: configure UDP GRO forwarding

--- hosts/common/optional/tailscale.nix
+++ hosts/common/optional/tailscale.nix
@@ -1,4 +1,8 @@
-{ inputs, lib, pkgs, ... }:
+{ config, inputs, lib, pkgs, ... }:
+let
+  cfg = config.services.tailscale;
+  tuneForwarding = cfg.enable && builtins.elem cfg.useRoutingFeatures ["both" "server"];
+in
 {
   services.tailscale = {
     enable = true;
@@ -9,4 +13,8 @@
       system = pkgs.system;
     }).tailscale;
   };
+
+  services.udev.extraRules = lib.mkIf tuneForwarding ''
+    ACTION=="add", SUBSYSTEM=="net", RUN+="${pkgs.ethtool}/bin/ethtool -K $name rx-udp-gro-forwarding on rx-gro-list off"
+  '';
 }
