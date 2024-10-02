f37d348765036b7e3350c9ff49e6b00a22435622
natanbc <natanbc@users.noreply.github.com>
2024-10-02 06:04:08 +0000

    nas: tailscale: advertise subnet

--- hosts/nas/services/tailscale.nix
+++ hosts/nas/services/tailscale.nix
@@ -1,7 +1,7 @@
 { ... }:
 {
   services.tailscale = {
-    extraSetFlags = ["--advertise-exit-node"];
+    extraSetFlags = ["--advertise-exit-node" "--advertise-routes=192.168.0.0/23"];
     useRoutingFeatures = "both";
   };
 }
