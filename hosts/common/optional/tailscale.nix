ab5581af1fbe3495d0db679b4bc80c15a35b683a
natanbc <natanbc@users.noreply.github.com>
2024-10-09 23:41:30 +0000

    common: add advertiseExitNode and advertiseSubnets tailscale options

--- hosts/common/optional/tailscale.nix
+++ hosts/common/optional/tailscale.nix
@@ -1,20 +1,46 @@
 { config, inputs, lib, pkgs, ... }:
 let
   cfg = config.services.tailscale;
+  defaultServerRouting = cfg.advertiseSubnets != [] || cfg.advertiseExitNode;
+  routeString = lib.strings.concatStringsSep "," cfg.advertiseSubnets;
   tuneForwarding = cfg.enable && builtins.elem cfg.useRoutingFeatures ["both" "server"];
 in
 {
-  services.tailscale = {
-    enable = true;
-    openFirewall = true;
-    useRoutingFeatures = lib.mkDefault "client";
+  options.services.tailscale = {
+    advertiseExitNode = lib.mkOption {
+      description = ''
+        Whether to advertise the machine as an exit node.
+      '';
+      default = [];
+      type = lib.types.bool;
+    };
 
-    package = (import inputs.nixpkgs-unstable {
-      system = pkgs.system;
-    }).tailscale;
+    advertiseSubnets = lib.mkOption {
+      description = ''
+        Extra subnets to advertise as routes.
+      '';
+      default = [];
+      type = lib.types.listOf lib.types.str;
+    };
   };
 
-  services.udev.extraRules = lib.mkIf tuneForwarding ''
-    ACTION=="add", SUBSYSTEM=="net", RUN+="${pkgs.ethtool}/bin/ethtool -K $name rx-udp-gro-forwarding on rx-gro-list off"
-  '';
+  config = {
+    services.tailscale = {
+      enable = true;
+      extraSetFlags = (
+        (lib.optional cfg.advertiseExitNode "--advertise-exit-node") ++
+        (lib.optional (cfg.advertiseSubnets != []) "--advertise-routes=${routeString}")
+      );
+      openFirewall = true;
+      useRoutingFeatures = lib.mkDefault (if defaultServerRouting then "both" else "client");
+
+      package = (import inputs.nixpkgs-unstable {
+        system = pkgs.system;
+      }).tailscale;
+    };
+
+    services.udev.extraRules = lib.mkIf tuneForwarding ''
+      ACTION=="add", SUBSYSTEM=="net", RUN+="${pkgs.ethtool}/bin/ethtool -K $name rx-udp-gro-forwarding on rx-gro-list off"
+    '';
+  };
 }
