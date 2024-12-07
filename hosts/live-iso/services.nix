c500b6f92efb4139f3036b2eaa2286eb152914bb
natanbc <natanbc@users.noreply.github.com>
2024-12-07 23:58:45 +0000

    live-iso: autostart serial console

--- hosts/live-iso/services.nix
+++ hosts/live-iso/services.nix
@@ -4,7 +4,12 @@
     lidSwitchDocked = "ignore";
     lidSwitchExternalPower = "ignore";
   };
+  services.udev.extraRules = ''
+    ACTION=="add", SUBSYSTEM=="tty", KERNEL=="ttyACM*", ENV{SYSTEMD_WANTS}+="serial-getty@%k.service"
+    ACTION=="add", SUBSYSTEM=="tty", KERNEL=="ttyUSB*", ENV{SYSTEMD_WANTS}+="serial-getty@%k.service"
+  '';
 
   networking.firewall.enable = false;
+
 }
 
