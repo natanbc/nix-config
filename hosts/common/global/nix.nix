3e8bb5f846da0f32102a07c01ee9bfd06f10a4cd
natanbc <natanbc@users.noreply.github.com>
2024-12-07 22:24:22 +0000

    all: remove repl-flake feature (breaks build on 24.11)

--- hosts/common/global/nix.nix
+++ hosts/common/global/nix.nix
@@ -3,7 +3,7 @@
   nix = {
     settings = {
       auto-optimise-store = lib.mkDefault true;
-      experimental-features = [ "nix-command" "flakes" "repl-flake" ];
+      experimental-features = [ "nix-command" "flakes" ];
     };
 
     gc = {
