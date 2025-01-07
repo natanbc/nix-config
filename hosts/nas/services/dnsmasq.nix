d549f4da50668d0428fbbc898d9af15737a2d727
natanbc <natanbc@users.noreply.github.com>
2025-01-07 05:09:25 +0000

    nas: dnsmasq: don't use overlapping subnets between CX3 ports, set dhcp range to .10-.200

--- hosts/nas/services/dnsmasq.nix
+++ hosts/nas/services/dnsmasq.nix
@@ -23,7 +23,7 @@
       };
       enp1s0d1 = {
         ipv4.addresses = [{
-          address = "10.0.0.2";
+          address = "10.0.1.1";
           prefixLength = 24;
         }];
         mtu = 9000;
@@ -47,9 +47,9 @@
       interface = [ "enp1s0" "enp1s0d1" "enp7s0" ];
 
       dhcp-range = [
-        "enp1s0,10.0.0.3,10.0.0.200,12h"
-        "enp1s0d1,10.0.0.3,10.0.0.200,12h"
-        "enp7s0,192.168.128.100,192.168.128.200,12h"
+        "enp1s0,10.0.0.10,10.0.0.200,12h"
+        "enp1s0d1,10.0.1.10,10.0.1.200,12h"
+        "enp7s0,192.168.128.10,192.168.128.200,12h"
       ];
       dhcp-option = [
         # Don't send default route
