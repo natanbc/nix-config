181d6aa57b6937052f1712e2c06caf917ca68d42
natanbc <natanbc@users.noreply.github.com>
2025-01-08 01:39:16 +0000

    nas: dnsmasq: add IPv6

--- hosts/nas/services/dnsmasq.nix
+++ hosts/nas/services/dnsmasq.nix
@@ -3,14 +3,14 @@
     firewall.interfaces = {
       enp1s0 = {
         allowedTCPPorts = [ 139 445 5201 ];
-        allowedUDPPorts = [ 67 5201 ];
+        allowedUDPPorts = [ 67 547 5201 ];
       };
       enp1s0d1 = {
         allowedTCPPorts = [ 139 445 5201 ];
-        allowedUDPPorts = [ 67 5201 ];
+        allowedUDPPorts = [ 67 547 5201 ];
       };
       enp7s0 = {
-        allowedUDPPorts = [ 67 ];
+        allowedUDPPorts = [ 67 547 ];
       };
     };
     interfaces = {
@@ -19,12 +19,20 @@
           address = "10.0.0.1";
           prefixLength = 24;
         }];
+        ipv6.addresses = [{
+          address = "fd4d:b5dc:3f9b:0001::1";
+          prefixLength = 64;
+        }];
         mtu = 9000;
       };
       enp1s0d1 = {
         ipv4.addresses = [{
           address = "10.0.1.1";
           prefixLength = 24;
+        }];
+        ipv6.addresses = [{
+          address = "fd4d:b5dc:3f9b:0002::1";
+          prefixLength = 64;
         }];
         mtu = 9000;
       };
@@ -33,6 +41,10 @@
         ipv4.addresses = [{
           address = "192.168.128.1";
           prefixLength = 24;
+        }];
+        ipv6.addresses = [{
+          address = "fd4d:b5dc:3f9b:0003::1";
+          prefixLength = 64;
         }];
       };
     };
@@ -43,6 +55,7 @@
     resolveLocalQueries = false;
     settings = {
       bind-interfaces = true;
+      enable-ra = true;
       except-interface = [ "lo" ];
       interface = [ "enp1s0" "enp1s0d1" "enp7s0" ];
 
@@ -50,15 +63,25 @@
         "enp1s0,10.0.0.10,10.0.0.200,12h"
         "enp1s0d1,10.0.1.10,10.0.1.200,12h"
         "enp7s0,192.168.128.10,192.168.128.200,12h"
+
+        "enp1s0,::1:0,::1:ff,constructor:enp1s0,12h"
+        "enp1s0d1,::1:0,::1:ff,constructor:enp1s0d1,12h"
+        "enp7s0,::1:0,::1:ff,constructor:enp7s0,12h"
       ];
       dhcp-option = [
         # Don't send default route
-        "3"
+        "option:router"
         # Don't send DNS server
-        "6"
-        # MTU
-        "enp1s0,26,9000"
-        "enp1s0d1,26,9000"
+        "option:dns-server"
+        "option6:dns-server"
+
+        "enp1s0,option:mtu,9000"
+        "enp1s0d1,option:mtu,9000"
+      ];
+      ra-param = [
+        "enp1s0,mtu:9000,86400,0"
+        "enp1s0d1,mtu:9000,86400,0"
+        "enp7s0,86400,0"
       ];
     };
   };
