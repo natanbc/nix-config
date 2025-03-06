2a5e0becc35531307484db843e818863dc7eb5aa
natanbc <natanbc@users.noreply.github.com>
2025-03-06 00:32:05 +0000

    nas: rtorrent: open port, enable dht/pex/udp trackers

--- hosts/nas/services/rtorrent/default.nix
+++ hosts/nas/services/rtorrent/default.nix
@@ -1,10 +1,24 @@
 { config, ... }: {
   imports = [ ./nginx.nix ];
 
+  networking.firewall = {
+    allowedTCPPorts = [ 50001 ];
+    allowedUDPPorts = [
+      config.services.rtorrent.port
+      50001
+    ];
+  };
+
   services.rtorrent = {
     enable = true;
     downloadDir = config.fileSystems."/mnt/scratch".mountPoint + "/rtorrent";
-    openFirewall = false;
+    openFirewall = true;
+    configText = ''
+      dht.mode.set = auto
+      dht.port.set = 50001
+      protocol.pex.set = yes
+      trackers.use_udp.set = yes
+    '';
   };
 
   services.rutorrent = {
