8a9ab2cc8297d4049b034c2eed620be6998dd422
natanbc <natanbc@users.noreply.github.com>
2025-03-17 01:45:19 +0000

    xenia: add openFPGALoader

--- home/natan/xenia.nix
+++ home/natan/xenia.nix
@@ -1,3 +1,11 @@
+{ inputs, pkgs, ... }:
+let
+  unstable = import inputs.nixpkgs-unstable {
+    system = pkgs.system;
+  };
+in
 {
   imports = [ ./common ];
+
+  home.packages = [ unstable.openfpgaloader ];
 }
