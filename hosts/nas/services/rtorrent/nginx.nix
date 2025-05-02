638d12c76b1c3030ba5a28270ec35ae09eed9ca6
natanbc <natanbc@users.noreply.github.com>
2025-05-02 04:37:35 +0000

    nas: rtorrent: use basic auth

--- hosts/nas/services/rtorrent/nginx.nix
+++ hosts/nas/services/rtorrent/nginx.nix
@@ -1,9 +1,17 @@
-{
+{ config, ... }: {
+  age.secrets.rtorrent-auth = {
+    file = ./auth.age;
+    owner = "nginx";
+    group = "nginx";
+  };
+
   services.nginx.virtualHosts = {
     "rtorrent.natanbc.net" = {
       addSSL = true;
       enableACME = true;
       acmeRoot = null;
+
+      basicAuthFile = config.age.secrets.rtorrent-auth.path;
     };
   };
 }
