f8c815e3a0214dceef259bc7c75c4727fddb3092
natanbc <natanbc@users.noreply.github.com>
2025-05-09 02:31:50 +0000

    home: natan: more helix keybinds

--- home/natan/common/helix.nix
+++ home/natan/common/helix.nix
@@ -5,18 +5,39 @@
 
   programs.helix = {
     enable = true;
-    
+
     defaultEditor = true;
-    settings = {
-      keys.normal = {
-        "C-r" = "redo";
+    settings = let
+      move_keys = {
+        "C-r"     = "redo";
         "C-right" = [ "move_next_word_start" "move_char_right" "collapse_selection" ];
-        "C-left" =  [ "move_prev_word_start"                   "collapse_selection" ];
-        "ins" = "insert_mode";
-        ";"."q" = ":q";
-        ";"."w" = ":w";
-        ";"."x" = ":x";
+        "C-left"  = [ "move_prev_word_start"                   "collapse_selection" ];
+        "C-down"  = "move_line_down";
+        "C-up"    = "move_line_up";
       };
+    in {
+      keys.normal = move_keys // {
+       "backspace" = [ "move_char_left" "delete_selection" ];
+       "ins"       = "insert_mode";
+
+        ";" = {
+          "q" = ":q";
+          "w" = ":w";
+          "x" = ":x";
+        };
+        "d" = {
+          "d"    = ["extend_to_line_bounds" "yank_main_selection_to_clipboard" "delete_selection"];
+          "down" = ["select_mode" "extend_to_line_bounds" "extend_line_below" "yank_main_selection_to_clipboard" "delete_selection" "normal_mode"];
+          "up"   = ["select_mode" "extend_to_line_bounds" "extend_line_above" "yank_main_selection_to_clipboard" "delete_selection" "normal_mode"];
+        };
+        "y" = {
+          "y"    = ["extend_to_line_bounds" "yank_main_selection_to_clipboard" "normal_mode" "collapse_selection"];
+          "down" = ["select_mode" "extend_to_line_bounds" "extend_line_below" "yank_main_selection_to_clipboard" "collapse_selection" "normal_mode"];
+          "up"   = ["select_mode" "extend_to_line_bounds" "extend_line_above" "yank_main_selection_to_clipboard" "collapse_selection" "normal_mode"];
+
+        };
+      };
+      keys.insert = move_keys;
     };
   };
 }
