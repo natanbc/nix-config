92e007e1675ef6d971e58a39997b06b6eb1ebfc9
natanbc <natanbc@users.noreply.github.com>
2025-06-29 20:05:08 +0000

    nas: oauth2-proxy: use proxy protocol to preserve real client IPs

--- hosts/nas/services/oauth2-proxy/default.nix
+++ hosts/nas/services/oauth2-proxy/default.nix
@@ -1,4 +1,7 @@
 { config, pkgs, ... }:
+let
+  domain = "oauth2-proxy-nas.natanbc.net";
+in
 {
   imports = [
     ../../../common/optional/oauth2-proxy.nix
@@ -6,14 +9,29 @@
 
   age.secrets.oauth2-proxy.file = ./secrets.nix;
 
-  services.nginx.virtualHosts."${config.services.oauth2-proxy.nginx.domain}" = {
+  networking.firewall.allowedTCPPorts = [ 444 ];
+  security.acme.certs."${domain}".group = config.services.nginx.group;
+
+  services.nginx.virtualHosts."${domain}" = {
     addSSL = true;
-    enableACME = true;
+    useACMEHost = domain;
+
+    listen = [
+      { addr = "0.0.0.0"; port = 444; proxyProtocol = true; ssl = true; }
+      { addr = "[::0]";   port = 444; proxyProtocol = true; ssl = true; }
+    ];
+
+    extraConfig = ''
+      set_real_ip_from 100.64.0.0/10;
+      set_real_ip_from fd7a:115c:a1e0::/48;
+      real_ip_header proxy_protocol;
+    '';
   };
 
   services.oauth2-proxy = {
     clientID = "oauth2-proxy-nas";
-    nginx.domain = "oauth2-proxy-nas.natanbc.net";
+    nginx.domain = domain;
+    reverseProxy = true;
   };
 
   systemd.services.oauth2-proxy.after = [
