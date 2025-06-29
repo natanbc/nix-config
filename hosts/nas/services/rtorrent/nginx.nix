a7cf455dbaa68a5a0941d61111977917fd365a1b
natanbc <natanbc@users.noreply.github.com>
2025-06-29 20:02:31 +0000

    nas: rtorrent: use proxy protocol to preserve real client IPs

--- hosts/nas/services/rtorrent/nginx.nix
+++ hosts/nas/services/rtorrent/nginx.nix
@@ -4,12 +4,24 @@
     message = "rtorrent authentication is handled by oauth2-proxy";
   }];
 
+  networking.firewall.allowedTCPPorts = [ 444 ];
+  security.acme.certs."rtorrent.natanbc.net".group = config.services.nginx.group;
+
   services.nginx.virtualHosts = {
     "rtorrent.natanbc.net" = {
       addSSL = true;
-      enableACME = true;
-      acmeRoot = null;
+      useACMEHost = "rtorrent.natanbc.net";
 
+      listen = [
+        { addr = "0.0.0.0"; port = 444; proxyProtocol = true; ssl = true; }
+        { addr = "[::0]";   port = 444; proxyProtocol = true; ssl = true; }
+      ];
+
+      extraConfig = ''
+        set_real_ip_from 100.64.0.0/10;
+        set_real_ip_from fd7a:115c:a1e0::/48;
+        real_ip_header proxy_protocol;
+      '';
     };
   };
 
